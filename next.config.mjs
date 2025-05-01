4812454cd315d79a8c9d81d9b20bcc79e9827073
Errold Núñez <errold222@gmail.com>
2025-05-01 11:37:33 -0600

    Corrige configuración de Next.js en Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    output: "standalone", // Permite ejecutar Next.js correctamente en Vercel
+    experimental: {
+        appDir: true,
+    },
+};
 
 export default nextConfig;
